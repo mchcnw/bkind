4cb318e46a295d4195babc4c57fad56310ff8d08
michael.conway <michael.conway@seachange-intl.com>
2016-11-23 22:50:53 +0000

    updated model

--- app/Models/Story.cs
+++ app/Models/Story.cs
@@ -1,13 +1,26 @@
+using System;
+
 namespace WebApplication{
     public class Story{
 
-        public string Story { get; set; }
+        public int Id { get; private set; }
         public string Title { get; set; }
         public string Content { get; set; }
         public int AuthorId { get; set; }
         public int State { get; set; }
+        public DateTime CreatedDate { get; set; }
 
+        public Story (int id)
+        {
+          Id = id;
+        }
 
+        public Story ()
+        {
+          // for EF
+        }
+
+    }
     public enum State{
         Active,
         InActive,
@@ -16,4 +29,6 @@ namespace WebApplication{
     }
 
 }
+
+
 
